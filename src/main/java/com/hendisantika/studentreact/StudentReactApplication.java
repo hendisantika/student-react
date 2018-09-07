26b21c343dd20ebb8075d8fea5b767bec358f54d
Hendi Santika <hendisantika@gmail.com>
2018-09-08 05:52:58 +0700

    first commit

---
+++ src/main/java/com/hendisantika/studentreact/StudentReactApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.studentreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentReactApplication.class, args);
+    }
+}
